8209047d16818c3893c5db8d5b14c837ee86338b
Michael Dudley <michael.dudley@improving.com>
2018-03-28 17:15:48 -0500

    preparing the OpenIdConnect example

--- IdentityServer/Startup.cs
+++ IdentityServer/Startup.cs
@@ -17,6 +17,7 @@ namespace IdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddInMemoryApiResources(Config.GetApiResources())
@@ -33,6 +34,8 @@ namespace IdentityServer
             }
 
             app.UseIdentityServer();
+            app.UseStaticFiles();
+            app.UseMvcWithDefaultRoute();
         }
     }
 
